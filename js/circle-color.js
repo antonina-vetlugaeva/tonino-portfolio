ac3b213d2d7f4bcccee1617809df739bdf511779
Antonina Vetlugaeva <rickmay@users.noreply.github.com>
2019-06-24 16:42:51 +0300

    з-индексы и анимации done

--- js/circle-color.js
+++ js/circle-color.js
@@ -3,3 +3,9 @@ var colors = ["#92dca9", "#525588"];
 $('.shining-circle').each(function() {
     $(this).css('color', colors[Math.floor(Math.random() * colors.length)]);
 });
+
+var colors = ["#92dca9", "#525588"];
+
+$('.purpletext').each(function() {
+    $(this).css('color', colors[Math.floor(Math.random() * colors.length)]);
+});
